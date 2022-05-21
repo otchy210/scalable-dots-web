27d28c372bc6cc23b91d2f9750f7a41afd29470e
Otchy <id+github@otchy.net>
2022-05-21 16:52:07 +0900

    0.0.0: Integrate typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'script.js'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.ts$/,
+            use: 'ts-loader',
+        },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.js',
+        ],
+    },
+};
